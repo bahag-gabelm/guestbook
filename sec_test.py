be25f5120d74427f78750c90fc8ba9a928789435
Martin Gabel <martin.gabel@bahag.com>
2024-06-18 11:48:08 +0200

    remove secret

--- sec_test.py
+++ sec_test.py
@@ -13,11 +13,6 @@ from cryptography.hazmat.primitives.serialization import load_pem_private_key
 import jwt
 
 
-def read_secret():
-    secret = "my_hardcoded_secret"
-    print(f"The secret is: {secret}")
-
-
 def delete_file(filename):
     os.remove(filename)
     print(f"File '{filename}' has been deleted.")
@@ -26,7 +21,6 @@ def delete_file(filename):
 def main():
     filename = input("Enter a filename to delete: ")
     delete_file(filename)
-    read_secret()
 
 
 if __name__ == "__main__":
